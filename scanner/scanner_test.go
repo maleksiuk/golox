35bad16df2ee10bbdf3b8b84f67df4d6041bf22c
Mike Aleksiuk <maleksiuk@gmail.com>
2020-06-26 08:37:27 -0700

    Clean up scanner tests.

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -7,78 +7,55 @@ import (
 	"github.com/maleksiuk/golox/tokens"
 )
 
+func assertSliceLength(t *testing.T, tokenSlice []tokens.Token, expectedLength int) {
+	if len(tokenSlice) != expectedLength {
+		t.Errorf("Slice length should be %d, was %d", expectedLength, len(tokenSlice))
+	}
+}
+
+func assertTokenType(t *testing.T, token tokens.Token, expectedTokenType tokens.TokenType) {
+	if token.TokenType != expectedTokenType {
+		t.Errorf("Token should be of type %v, was %v", expectedTokenType, token.TokenType)
+	}
+}
+
 func TestScanTokens(t *testing.T) {
 	tokenSlice := ScanTokens("()", &errorreport.ErrorReport{})
 
-	if len(tokenSlice) != 3 {
-		t.Errorf("len(tokens) should be 3, was %d", len(tokenSlice))
-	}
+	assertSliceLength(t, tokenSlice, 3)
 
-	if tokenSlice[0].TokenType != tokens.LeftParen {
-		t.Errorf("First token should be of type %v, was %v", tokens.LeftParen, tokenSlice[0].TokenType)
-	}
-
-	if tokenSlice[1].TokenType != tokens.RightParen {
-		t.Errorf("Second token should be of type %v, was %v", tokens.RightParen, tokenSlice[1].TokenType)
-	}
-
-	if tokenSlice[2].TokenType != tokens.EOF {
-		t.Errorf("Last token should be of type %v, was %v", tokens.EOF, tokenSlice[2].TokenType)
-	}
+	assertTokenType(t, tokenSlice[0], tokens.LeftParen)
+	assertTokenType(t, tokenSlice[1], tokens.RightParen)
+	assertTokenType(t, tokenSlice[2], tokens.EOF)
 }
 
 func TestScanTokensWithMultipleCharacters(t *testing.T) {
 	bangTokenSlice := ScanTokens("!", &errorreport.ErrorReport{})
 	bangEqualTokenSlice := ScanTokens("!=", &errorreport.ErrorReport{})
 
-	if len(bangTokenSlice) != 2 {
-		t.Errorf("len(bangTokenSlice) should be 2, was %d", len(bangTokenSlice))
-	}
+	assertSliceLength(t, bangTokenSlice, 2)
+	assertSliceLength(t, bangEqualTokenSlice, 2)
 
-	if len(bangEqualTokenSlice) != 2 {
-		t.Errorf("len(bangEqualTokenSlice) should be 2, was %d", len(bangEqualTokenSlice))
-	}
-
-	if bangTokenSlice[0].TokenType != tokens.Bang {
-		t.Errorf("bangTokenSlice token should be of type %v, was %v", tokens.Bang, bangTokenSlice[0].TokenType)
-	}
-
-	if bangEqualTokenSlice[0].TokenType != tokens.BangEqual {
-		t.Errorf("bangEqualTokenSlice token should be of type %v, was %v", tokens.BangEqual, bangEqualTokenSlice[0].TokenType)
-	}
+	assertTokenType(t, bangTokenSlice[0], tokens.Bang)
+	assertTokenType(t, bangEqualTokenSlice[0], tokens.BangEqual)
 }
 
 func TestScanComments(t *testing.T) {
 	commentTokenSlice := ScanTokens("// This should be ignored", &errorreport.ErrorReport{})
 	slashTokenSlice := ScanTokens("/*", &errorreport.ErrorReport{})
 
-	if len(commentTokenSlice) != 1 {
-		t.Errorf("len(commentTokenSlice) should be 1, was %d", len(commentTokenSlice))
-	}
+	assertSliceLength(t, commentTokenSlice, 1)
+	assertTokenType(t, commentTokenSlice[0], tokens.EOF)
 
-	if commentTokenSlice[0].TokenType != tokens.EOF {
-		t.Errorf("commentTokenSlice token should be of type %v, was %v", tokens.EOF, commentTokenSlice[0].TokenType)
-	}
-
-	if len(slashTokenSlice) != 3 {
-		t.Errorf("len(slashTokenSlice) should be 1, was %d", len(slashTokenSlice))
-	}
-
-	if slashTokenSlice[0].TokenType != tokens.Slash {
-		t.Errorf("slashTokenSlice token should be of type %v, was %v", tokens.Slash, slashTokenSlice[0].TokenType)
-	}
+	assertSliceLength(t, slashTokenSlice, 3)
+	assertTokenType(t, slashTokenSlice[0], tokens.Slash)
 }
 
 func TestScanMultipleLines(t *testing.T) {
 	tokenSlice := ScanTokens("()\n!=", &errorreport.ErrorReport{})
 
-	if len(tokenSlice) != 4 {
-		t.Errorf("len(tokenSlice) should be 4, was %d", len(tokenSlice))
-	}
-
-	if tokenSlice[2].TokenType != tokens.BangEqual {
-		t.Errorf("commentTokenSlice token should be of type %v, was %v", tokens.BangEqual, tokenSlice[0].TokenType)
-	}
+	assertSliceLength(t, tokenSlice, 4)
+	assertTokenType(t, tokenSlice[2], tokens.BangEqual)
 }
 
 // TODO: Test that line count is being incremented, including in the comment case.
