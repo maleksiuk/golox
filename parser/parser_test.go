b6a24d652ac526a7f55a6b7bcafa6c27d7ddcf33
Mike Aleksiuk <maleksiuk@gmail.com>
2020-07-13 10:56:16 -0700

    Parser: increase test coverage a bit.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,17 +16,39 @@ func assertAST(t *testing.T, expression expr.Expr, expectedTree string) {
 	}
 }
 
-func TestParse(t *testing.T) {
-
+func TestParseArithmeticAndComparison(t *testing.T) {
+	// (123.9 + 92) >= 5 * -9
 	tokens := []toks.Token{
 		{TokenType: toks.LeftParen, Lexeme: "(", Literal: nil, Line: 0},
 		{TokenType: toks.Number, Lexeme: "123.9", Literal: 123.9, Line: 0},
 		{TokenType: toks.Plus, Lexeme: "+", Literal: nil, Line: 0},
 		{TokenType: toks.Number, Lexeme: "92", Literal: 92, Line: 0},
 		{TokenType: toks.RightParen, Lexeme: ")", Literal: nil, Line: 0},
+		{TokenType: toks.GreaterEqual, Lexeme: ">=", Literal: nil, Line: 0},
+		{TokenType: toks.Number, Lexeme: "5", Literal: 5, Line: 0},
+		{TokenType: toks.Star, Lexeme: "*", Literal: nil, Line: 0},
+		{TokenType: toks.Number, Lexeme: "-9", Literal: -9, Line: 0},
 		{TokenType: toks.EOF, Lexeme: "", Literal: nil, Line: 0},
 	}
 
 	expression := Parse(tokens, &errorreport.ErrorReport{})
-	assertAST(t, expression, "(group (+ 123.9 92))")
+	assertAST(t, expression, "(>= (group (+ 123.9 92)) (* 5 -9))")
+}
+
+func TestParseUnariesStringsAndBooleans(t *testing.T) {
+	// !("str1" == "str2") == false
+	tokens := []toks.Token{
+		{TokenType: toks.Bang, Lexeme: "!", Literal: nil, Line: 0},
+		{TokenType: toks.LeftParen, Lexeme: "(", Literal: nil, Line: 0},
+		{TokenType: toks.String, Lexeme: "\"str1\"", Literal: "str1", Line: 0},
+		{TokenType: toks.EqualEqual, Lexeme: "==", Literal: nil, Line: 0},
+		{TokenType: toks.String, Lexeme: "\"str2\"", Literal: "str2", Line: 0},
+		{TokenType: toks.RightParen, Lexeme: ")", Literal: nil, Line: 0},
+		{TokenType: toks.EqualEqual, Lexeme: "==", Literal: nil, Line: 0},
+		{TokenType: toks.False, Lexeme: "false", Literal: nil, Line: 0},
+		{TokenType: toks.EOF, Lexeme: "", Literal: nil, Line: 0},
+	}
+
+	expression := Parse(tokens, &errorreport.ErrorReport{})
+	assertAST(t, expression, "(== (! (group (== str1 str2))) false)")
 }
